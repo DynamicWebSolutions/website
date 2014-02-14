a807415921d81d84d04cf943fbd7f5d7e8d1964f
Matthew Rosenberg <matt@dynamicwebsolutions.com>
2014-02-14 15:07:51 -0500

    - open map in new tab

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 
 <!--==============================footer=================================-->
 <footer>
-<a href="https://www.google.com/maps/place/Dynamic+Web+Solutions/@37.5350131,-77.4239607,17z/data=!4m2!3m1!1s0x89b1111974bd9c77:0x2f4e617853a91d1b"><div id="locator-map"></div></a>
+<a href="https://www.google.com/maps/place/Dynamic+Web+Solutions/@37.5350131,-77.4239607,17z/data=!4m2!3m1!1s0x89b1111974bd9c77:0x2f4e617853a91d1b" title="Open in Google Maps" target="_blank"><div id="locator-map"></div></a>
   <div class="footer-2">
     <div class="container">
       	<div class="row">
